d33c0dc919e45ccf5d80a3b1b5d6449ac802bab0
Savitha V <savi3019@global.chainsys.com>
2022-03-08 22:55:05 -0800

    second commit

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+const share_badge = document.querySelector('.content-footer')
+
+let open = false
+
+// eslint-disable-next-line no-unused-vars
+function share () {
+  open = !open
+  if (open) {
+    share_badge.classList.remove('off')
+    share_badge.classList.add('on')
+  } else {
+    share_badge.classList.add('off')
+    share_badge.classList.remove('on')
+  }
+}
